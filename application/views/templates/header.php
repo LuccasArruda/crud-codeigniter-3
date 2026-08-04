a5ac50c991d903ed9dc718fecc551bd8bd56c187
Luccas Arruda <luccasarruda@hotmail.com.br>
2026-08-04 10:26:27 -0300

    feat(icon): adicionar ícone ao site

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo $title; ?></title>
+        <link rel="icon" type="image/x-icon" href="<?php echo base_url('public/assets/img/icons/favicon.svg'); ?>">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,200,0..1,-25..200"/>	
         <link rel="stylesheet" href="<?php echo base_url('public/assets/css/common.css'); ?>">
